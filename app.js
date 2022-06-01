6fc388fc045b316b8f28c942ecc72813ca55a340
Oshamaru <callMeSam808@gmail.com>
2022-06-01 10:51:53 -1000

    Add post to form, save data to variables.

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const bodyParser = require('body-parser');
 
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/signup.html');
 });
+
+
+app.post('/', (req, res) => {
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+  res.send(`Thanks ${firstName}!`);
+})
+
 
 app.listen(port, () => {
   console.log(`Newsletter-Signup is listening on port ${port}.`);
