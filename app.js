73d219aca98617b741cdcb061e262c5a045ec02f
Oshamaru <callMeSam808@gmail.com>
2022-06-01 15:19:06 -1000

    Create batch subscribe for lists/audiences with mailchimp api.

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const https = require("https");
 
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
+
+
+const apiKey = "5f9aea9939823f77edfcdef5c63785bc-us14";
+const server = "us14";
+const listID = "8073f9606c"
 
 
 app.get('/', (req, res) => {
@@ -16,8 +22,45 @@ app.post('/', (req, res) => {
   const firstName = req.body.firstName;
   const lastName = req.body.lastName;
   const email = req.body.email;
-  res.send(`Thanks ${firstName}!`);
-})
+  
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = `https://${server}.api.mailchimp.com/3.0/lists/${listID}`;
+
+  const options = {
+    method: "POST",
+    auth: `callmesam808:${apiKey}`
+  }
+
+  const request = https.request(url, options, (response) => {
+    if (response.statusCode === 200) {
+      res.send("Success!");
+    } else {
+      res.send("Submission failed. Please try again!");
+    }
+    
+    response.on("data", (data) => {
+      console.log(JSON.parse(data));
+    })
+  })
+  
+  request.write(jsonData);
+  request.end();
+
+});
 
 
 app.listen(port, () => {
